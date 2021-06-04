6df8e07fc7bac92296bbc84f4c7dc8bc7f717e15
jgiles <jgiles12345@gmail.com>
2021-06-04 17:44:55 +0000

    code and tests complete

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,4 +1,5 @@
 from argparse import Action, ArgumentParser
+import sys
 known_drivers = ['local', 's3']
 class DriverAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -11,11 +12,32 @@ def create_parser():
     parser = ArgumentParser(description="""
     Back up PostgreSQL databases locally or to AWS S3.
     """)
-    parser.add_argument("url", help="URL of database to backup")
-    parser.add_argument("--driver",
+    parser.add_argument('url', help="URL of database to backup")
+    parser.add_argument('--driver', '-d',
             help="how & where to store backup",
             nargs=2,
             action=DriverAction,
+            metavar=('driver', 'destination'),
             required=True)
     return parser
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    #print(f"{args.url=}")
+    #sys.exit()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT:%H:%M:%S", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in S3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {args.destination}")
+        storage.local(dump.stdout, outfile)
 
